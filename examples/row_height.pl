1570de46d422c599bf9c4494f2f90d3e9eb5a34c
Desislav Kamenov <desislav@kamenov.biz>
2014-12-07 17:54:59 +0000

    Examples have been formatted. One new example added (header.pl) for demonstrating how header row can be added to multiple pages.

--- examples/row_height.pl
+++ examples/row_height.pl
@@ -14,32 +14,32 @@ $pdf->mediabox('A4');
 # A4 as defined by PDF::API2 is h=842 w=545 for portrait
 
 my $data = [];
-# some data to layout
- 
-foreach my $num(1..25){
-    push(@$data , ['foo'.$num,'bar'.$num]);    
-}
 
+# some data to layout
+
+foreach my $num ( 1 .. 25 ) {
+	push( @$data, [ 'foo' . $num, 'bar' . $num ] );
+}
 
 # build the table layout
 $pdftable->table(
 
-    # required params
-    $pdf,
-    $page,
-    $data,
-    x       => 10,
-    w       => 150,
-    start_y => 750,
-    next_y  => 700,
-    start_h => 200,
-    next_h  => 500,
+	# required params
+	$pdf,
+	$page,
+	$data,
+	x       => 10,
+	w       => 150,
+	start_y => 750,
+	next_y  => 700,
+	start_h => 200,
+	next_h  => 500,
 
-    # some optional params
-    border          => 1,
-    font_size       => 10,
-    max_word_length => 15,
-    padding         => 5,
-    row_height      => 2,
+	# some optional params
+	border          => 1,
+	font_size       => 10,
+	max_word_length => 15,
+	padding         => 5,
+	row_height      => 30,
 );
 $pdf->saveas();
